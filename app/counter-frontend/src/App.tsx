c570a422cf241eb0ccfa254f89f1ee2750fa858a
raghav-rama <hackerer528@gmail.com>
2023-01-24 16:29:57 +0530

    feat: add navbar

--- app/counter-frontend/src/App.tsx
+++ app/counter-frontend/src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import * as React from 'react';
+
+import { Box, AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { Wallet } from './components/SolanaMultiWallet';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Box component="nav" sx={{ flexGrow: 1 }}>
+        <AppBar position="absolute" sx={{ backgroundColor: "purple" }}>
+          <Toolbar>
+            <img className="Navbar-app-logo" src={require("./img/logo192.png")} alt="logo" height={30} width={30} />
+            <Typography variant="h4" component="h1" sx={{ flexGrow: 1, textAlign: "left", ml: 2 }}>
+              Counter Frontend
+            </Typography>
+            <Button variant="contained">
+              Connect
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +36,7 @@ function App() {
         >
           Learn React
         </a>
+        <Wallet />
       </header>
     </div>
   );
