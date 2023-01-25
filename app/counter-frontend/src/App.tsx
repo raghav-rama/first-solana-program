a36b3bb1c6f1d4af43ffc2115515d1f65d7aaf3f
raghav-rama <hackerer528@gmail.com>
2023-01-25 05:51:12 +0530

    feat: update UI

--- app/counter-frontend/src/App.tsx
+++ app/counter-frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { Box, AppBar, Toolbar, Typography, Button } from '@mui/material';
-import { Wallet } from './components/SolanaMultiWallet';
+import { Box, AppBar, Toolbar, Typography, Button, Grid } from "@mui/material";
+import { Wallet } from "./components/SolanaMultiWallet";
+import { Connection, Transaction, Keypair, clusterApiUrl } from "@solana/web3.js"
+import idl from "./idl.json";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -12,31 +14,63 @@ function App() {
       <Box component="nav" sx={{ flexGrow: 1 }}>
         <AppBar position="absolute" sx={{ backgroundColor: "purple" }}>
           <Toolbar>
-            <img className="Navbar-app-logo" src={require("./img/logo192.png")} alt="logo" height={30} width={30} />
-            <Typography variant="h4" component="h1" sx={{ flexGrow: 1, textAlign: "left", ml: 2 }}>
+            <img
+              className="Navbar-app-logo"
+              src={require("./img/logo192.png")}
+              alt="logo"
+              height={30}
+              width={30}
+            />
+            <Typography
+              variant="h4"
+              component="h1"
+              sx={{
+                flexGrow: 1,
+                textAlign: "left",
+                ml: 2,
+                fontFamily: "Roboto, sans-serif",
+              }}
+            >
               Counter Frontend
             </Typography>
-            <Button variant="contained">
-              Connect
-            </Button>
+            <Wallet />
           </Toolbar>
         </AppBar>
       </Box>
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Wallet />
+        <Box sx={{ display: "flex", gap: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Button variant="contained" sx={{ width: "90%" }}>Initialize</Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button variant="contained" sx={{ width: "90%" }}>Update</Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button variant="contained" sx={{ width: "90%" }}>Decrement</Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button variant="contained" sx={{ width: "90%" }}>Increment</Button>
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant="contained" sx={{ width: "90%" }} onClick={
+                async () => {
+                  const connection = new Connection(clusterApiUrl("devnet"));
+                  const version = await connection.getVersion();
+                  console.log("connection established to devnet", connection, version);
+                  const private_key = "2cbThFSkyEqvmznKPh9uX83TSXTzSwqJxho6z7ba3FiTwiAhq4CUqcrypq9WETGjAC3wDfhCwPcoAubLDyZ8NRpM";
+                  const encoded_priv_key = new TextEncoder().encode(private_key);
+                  console.log(encoded_priv_key);
+                  console.log(idl);
+                  const keypair = Keypair.fromSecretKey(encoded_priv_key);
+                  console.log(encoded_priv_key, keypair);
+                }
+              }>Establish Connection</Button>
+            </Grid>
+          </Grid>
+        </Box>
       </header>
     </div>
   );
