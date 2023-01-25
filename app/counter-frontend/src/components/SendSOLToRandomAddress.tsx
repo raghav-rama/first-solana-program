7b7da81ff78f93aa9324c589edb0d60009efd3ec
raghav-rama <hackerer528@gmail.com>
2023-01-25 16:55:58 +0530

    refactor: change button to material button

--- app/counter-frontend/src/components/SendSOLToRandomAddress.tsx
+++ app/counter-frontend/src/components/SendSOLToRandomAddress.tsx
@@ -1,41 +1,52 @@
-import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
-import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { Keypair, SystemProgram, Transaction } from '@solana/web3.js';
-import React, { FC, useCallback } from 'react';
+import { WalletNotConnectedError } from "@solana/wallet-adapter-base";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import React, { FC, useCallback } from "react";
+import { Button } from "@mui/material";
+// window.Buffer = window.Buffer || require("buffer").Buffer;
+
 
 export const SendSOLToRandomAddress: FC = () => {
-    const { connection } = useConnection();
-    const { publicKey, sendTransaction } = useWallet();
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
 
-    
-    const onClick = useCallback(async () => {
-        console.log(connection, publicKey);
-        if (!publicKey) throw new WalletNotConnectedError();
+  const onClick = useCallback(async () => {
+    console.log("Sending from:", publicKey?.toBase58());
+    const random = Keypair.generate().publicKey;
+    console.log("Sending to:", random.toBase58());
 
-        // 890880 lamports as of 2022-09-01
-        const lamports = await connection.getMinimumBalanceForRentExemption(0);
+    if (!publicKey) throw new WalletNotConnectedError();
 
-        const transaction = new Transaction().add(
-            SystemProgram.transfer({
-                fromPubkey: publicKey,
-                toPubkey: Keypair.generate().publicKey,
-                lamports,
-            })
-        );
+    // 890880 lamports as of 2022-09-01
+    const lamports = await connection.getMinimumBalanceForRentExemption(0);
 
-        const {
-            context: { slot: minContextSlot },
-            value: { blockhash, lastValidBlockHeight }
-        } = await connection.getLatestBlockhashAndContext();
-
-        const signature = await sendTransaction(transaction, connection, { minContextSlot });
-
-        await connection.confirmTransaction({ blockhash, lastValidBlockHeight, signature });
-    }, [publicKey, sendTransaction, connection]);
-
-    return (
-        <button onClick={onClick} disabled={!publicKey}>
-            Send SOL to a random address!
-        </button>
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: publicKey,
+        toPubkey: random,
+        lamports,
+      })
     );
+
+    const {
+      context: { slot: minContextSlot },
+      value: { blockhash, lastValidBlockHeight },
+    } = await connection.getLatestBlockhashAndContext();
+
+    const signature = await sendTransaction(transaction, connection, {
+      minContextSlot,
+    });
+
+    await connection.confirmTransaction({
+      blockhash,
+      lastValidBlockHeight,
+      signature,
+    });
+  }, [publicKey, sendTransaction, connection]);
+
+  return (
+    <Button variant="contained" onClick={onClick} disabled={!publicKey}>
+      Send SOL to random address!
+    </Button>
+  );
 };
