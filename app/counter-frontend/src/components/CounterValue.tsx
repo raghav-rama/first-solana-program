26ba1cf28724796bb2d38ea0d17cd2e58ce2bd41
raghav-rama <hackerer528@gmail.com>
2023-01-27 03:29:02 +0530

    feat: improve logic

--- app/counter-frontend/src/components/CounterValue.tsx
+++ app/counter-frontend/src/components/CounterValue.tsx
@@ -1,42 +1,13 @@
 import * as React from "react";
-import { useAnchorWallet } from "@solana/wallet-adapter-react";
 import { Box, Paper, Typography } from "@mui/material";
-import { Program, AnchorProvider, Wallet } from "@project-serum/anchor";
-import { clusterApiUrl, Connection } from "@solana/web3.js";
-import { baseAccount } from "./Initialize";
-import IDL from "../idl/basic_1.json";
 
-let counter: number = 0;
-let account: any;
+interface CounterValueProps {
+  counter: number;
+  setCounter: React.Dispatch<React.SetStateAction<number>>;
+}
 
-const CounterValue: React.FC = () => {
-  const wallet = useAnchorWallet() as Wallet;
-  const network = clusterApiUrl("devnet");
-  const connection = new Connection(network, "processed");
-  const provider = new AnchorProvider(connection, wallet, {
-    preflightCommitment: "processed",
-  });
-  const JSON_IDL = JSON.parse(JSON.stringify(IDL));
-  const program = new Program(JSON_IDL, IDL.metadata.address, provider);
-
-  const [counterValue, setCounterValue] = React.useState<number>(0);
-
-  try {
-    const fetchCounterValue = async () => {
-      account = await program.account.myAccount.fetch(
-        baseAccount.publicKey
-      );
-      counter = parseInt(account.data);
-      console.log(counter);
-      setCounterValue(counter);
-    };
-    React.useEffect(() => {
-      const intervalId = setInterval(fetchCounterValue, 1000);
-      return () => clearInterval(intervalId);
-    }, []);
-  } catch (err) {
-    console.log("Error: ", err);
-  }
+const CounterValue: React.FC<CounterValueProps> = (props) => {
+  const { counter, } = props;
 
   return (
     <Box sx={{ mr: 5, ml: 5 }}>
@@ -53,4 +24,4 @@ const CounterValue: React.FC = () => {
   );
 };
 
-export { CounterValue, counter, account };
+export { CounterValue };
